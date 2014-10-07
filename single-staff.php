4d9e88e1814a38faaa74107df0ef47e25b79f7de
Evan Huntley <evan@evanhuntleycom>
2014-10-06 22:43:23 -0400

    Staff detail page

--- single-staff.php
+++ single-staff.php
@@ -7,17 +7,20 @@
 			<div class="profile-img">
 				<?php the_post_thumbnail('full');?>
 			</div>
-			<a class="button" href="http://www.djmikemorse.djintelligence.com/availability">Check Availability</a>
 			<div class="quote">
 	    		<?php
 					$quote_block_title = types_render_field("quote_block_title", array('raw'=>true));
 					$quote_block_text = types_render_field("quote_block_text", array('raw'=>true));  			
 	    		?>
+				<a class="button" href="http://www.djmikemorse.djintelligence.com/availability">Check Availability</a>
 				<h2><?php echo $quote_block_title; ?></h2>
 				<blockquote><?php echo $quote_block_text; ?></blockquote>
 			</div>
-			<h2>About</h2>
-			<?php the_content(); ?>
+			
+			<section class="main-content">
+				<h1>About</h1>
+				<?php the_content(); ?>
+			</section>
 
             <ul class="navigation">
                 <li class="older">
